3247427842ca87f5f0e08bc91a7a7c1983ef4a25
Brad B <bradley_bouchard@yahoo.com>
2016-07-16 12:17:39 -0400

    Adding Project Stuff to repo

---
+++ ch6.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>Laptop App 1</title>
+<style type="text/css">
+
+#ch6 {color: #E517CA;}
+
+.link {
+	font-size: 35px;
+	color: "#FFF";
+}
+
+.link:hover {
+	background-color: #FFF;
+	color: #000;	
+}
+
+</style>
+</head>
+
+<body style="background-color: #000;" >
+
+<a class="link" href="index.php">BACK</a>
+
+<?php
+
+// ---------------------------- START CHAPTER 6 LEARNING ----------------------------
+echo "<div id='ch6'>";
+echo "<br><br><<<---------------------------- START CHAPTER 6 LEARNING ---------------------------->>><br><br>";
+
+
+
+echo "<br><<<---------------------------- END CHAPTER 6 LEARNING ---------------------------->>>";
+// ---------------------------- END CHAPTER 6 LEARNING ----------------------------
+echo "</div>";
+
+?>
+
+</body>
+
+</html>
+
