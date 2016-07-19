ddf87d7105a79bc37cbab0e7a28c153a108af81a
Brad Bouchard <bradley.bouchard@its.ny.gov>
2016-07-19 15:41:56 -0400

    some message goes here

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>Laptop App 1</title>
+<style type="text/css">
+.nav {
+	font-size: 22px;
+	color: "#FFF";
+}
+.nav:hover {
+	background-color: #FFF;
+	color: #000;	
+}
+body {
+	color: #FFF;
+}
+</style>
+</head>
+
+<body style="background-color: #000;" >
+
+<?php
+	echo "HELLO WORLD!<br>";
+	echo "Welcome to Brad's PHP Learning Pages"
+?>
+
+<ul>
+	<li><a class="nav" href="ch3.php">Chapter 3</a></li>
+	<li><a class="nav" href="ch4.php">Chapter 4</a></li>
+	<li><a class="nav" href="ch5.php">Chapter 5</a></li>
+	<li><a class="nav" href="ch6.php">Chapter 6</a></li>
+	<li><a class="nav" href="ch7.php">Chapter 7</a></li>
+</ul>
+
+</body>
+
+</html>
