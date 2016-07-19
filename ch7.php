f6e4bae78cfa94cb0a0128e7bb50053c184bb7cf
Brad Bouchard <bradley.bouchard@its.ny.gov>
2016-07-19 14:46:03 -0400

    test

---
+++ ch7.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>Laptop App 1</title>
+<style type="text/css">
+
+#ch6 {color: #E517CA;}
+
+.link {
+	font-size: 35px;
+	color: "#FFF";
+}
+
+.link:hover {
+	background-color: #FFF;
+	color: #000;	
+}
+
+</style>
+</head>
+
+<body style="background-color: #000;" >
+
+<a class="link" href="index.php">BACK</a>
+
+<?php
+
+// ---------------------------- START CHAPTER 7 LEARNING ----------------------------
+echo "<div id='ch7'>";
+echo "<br><br><<<---------------------------- START CHAPTER 7 LEARNING ---------------------------->>><br><br>";
+
+
+
+echo "<br><<<---------------------------- END CHAPTER 7 LEARNING ---------------------------->>>";
+// ---------------------------- END CHAPTER 7 LEARNING ----------------------------
+echo "</div>";
+
+?>
+
+</body>
+
+</html>
+
